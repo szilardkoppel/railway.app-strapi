e7a582002ea583198687355f5262123e0ed9ac89
igor sharin <sharinigor1@gmail.com>
2025-08-25 17:42:22 +0900

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  host: env('HOST', '::'), // Изменено с '0.0.0.0' на '::'
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
